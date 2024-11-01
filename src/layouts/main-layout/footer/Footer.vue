669262098315da2e35baee2653bda803204ea23e
Syamim Hakimi <sym10115@gmail.com>
2024-11-01 15:07:38 +0800

    fix(footer): update footer

--- src/layouts/main-layout/footer/Footer.vue
+++ src/layouts/main-layout/footer/Footer.vue
@@ -10,32 +10,10 @@
       }"
     >
       <!--begin::Copyright-->
-      <div class="text-dark order-2 order-md-1">
-        <span class="text-muted fw-semobold me-1">2021&copy;</span>
-        <a
-          href="https://keenthemes.com/"
-          target="_blank"
-          class="text-gray-800 text-hover-primary"
-          >Keenthemes</a
-        >
+      <div class="text-muted fw-semibold order-2 order-md-1">
+        2024&copy; sym.
       </div>
       <!--end::Copyright-->
-
-      <!--begin::Menu-->
-      <ul class="menu menu-gray-600 menu-hover-primary fw-semobold order-1">
-        <li class="menu-item">
-          <a href="#" target="_blank" class="menu-link px-2">About</a>
-        </li>
-
-        <li class="menu-item">
-          <a href="#" target="_blank" class="menu-link px-2">Support</a>
-        </li>
-
-        <li class="menu-item">
-          <a href="#" target="_blank" class="menu-link px-2">Purchase</a>
-        </li>
-      </ul>
-      <!--end::Menu-->
     </div>
     <!--end::Container-->
   </div>
